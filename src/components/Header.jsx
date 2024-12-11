518140e17a99dbda6fbe3ae4a5048143f52f47a4
laurac <laurac0362@gmail.com>
2024-12-11 13:53:51 +0100

    barre recherche desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,13 +19,15 @@ function Header() {
                 <div className="header-lg-right">
 <div className="loupe-menu">
                 {/* Barre de recherche */}
-                <form className="d-none d-lg-flex search-bar ms-3">
+                <form className="d-none d-lg-flex search-bar ms-3 ">
                     <input 
                         type="text" 
                         className="form-control" 
                         aria-label="Search"
+                        placeholder="Rechercher"
+                        
                     />
-                    <i className="bi bi-search"></i> {/* Icône Bootstrap pour la loupe */}
+                    <i className="bi bi-search icon-loupe-d"></i> {/* Icône Bootstrap pour la loupe */}
                     
                 </form>
 <div className='search-menu-mobile'>
@@ -59,16 +61,16 @@ function Header() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-lg-auto">
                         <li className="nav-item">
-                            <Link className="nav-link mx-1" to="/">Bâtiment</Link>
+                            <Link className="nav-link mx-1" to="/batiment">Bâtiment</Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link mx-1" to="/services">Services</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link mx-1" to="/realisations">Fabrication</Link>
+                            <Link className="nav-link mx-1" to="/fabrication">Fabrication</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link mx-1" to="/blog">Alimentation</Link>
+                            <Link className="nav-link mx-1" to="/alimentation">Alimentation</Link>
                         </li>
                     </ul>
                 </div>
